6db71091c539a3eb78079a551887d1e780249f2c
SystemX <med.fellak@gmail.com>
2022-03-10 13:30:40 -0500

    Time: 65 ms (82.82%), Space: 42.8 MB (7.52%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var searchInsert = function(nums, target) {
+    /*
+    let i=0;
+    while (i < nums.length) {
+        if(target <= nums[i]) return i
+        else if(target > nums[i]) i++
+    }
+    return i
+    */
+
+    let i=0
+    while(i < nums.length){
+        if(target <= nums[i]) return i
+        else i++
+    }
+    return i
+};

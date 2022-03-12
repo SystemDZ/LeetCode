bfc4bd6ac44653912ffbd99a6fc09339979bf336
SystemX <med.fellak@gmail.com>
2022-03-11 20:01:14 -0500

    Time: 105 ms (64.74%), Space: 45.1 MB (53.77%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -3,20 +3,72 @@
  * @param {number} k
  * @return {number[]}
  */
-var topKFrequent = function(nums, k) {
+var topKFrequent = function(nums, k) { // Time O(2n + k) = O(n+k)
     let hash = {}
     let output = []
     for(let n of nums) hash[n] = hash[n] ? hash[n] + 1 : 1
-    let sortedArray = Object.entries(hash).sort((a,b)=>b[1]-a[1])
+    
+    let sortedArray = Object.entries(hash).sort((b,a)=>a[1]-b[1])
     for(let i=0; i<k; i++) output.push(sortedArray[i][0])
+    
     return output
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    /*
+    // Hash Table to add frequent numbers
+    let hash = {} // Space O(n)
+    // output
+    let output = []
+    // fill up our hash table with numbers frequency
+    for(let n of nums) hash[n] = hash[n] ? hash[n] + 1 : 1 // Time O(n)
+    // Sort our Hash table based on numbers frequency big to small
+    let sortedArray = Object.entries(hash).sort((a,b)=>b[1]-a[1]) // Time O(n) -  Space O(n)
+    // add max values less then key
+    for(let i=0; i<k; i++) output.push(sortedArray[i][0]) // Time O(k)
+    
+    
+    return output
+    
+    */
 };
 
 
 /*
-
 111123333333444
 1234
+
+1123456789 k=8
 
 1: 4
 2: 1
@@ -25,10 +77,8 @@ var topKFrequent = function(nums, k) {
 
 max = Math.max(max, cur)
 if(output.length <= k) output.push(max)
-
 first loop to count numbers
 second loop to find maximum of each number
 then add the most frequent nums to the array
 the array length is less then or equal to k
-
 */

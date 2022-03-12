cea48df0bd54e3cff4d9b4c1ef777948d6508ddc
SystemX <med.fellak@gmail.com>
2022-03-11 21:37:33 -0500

    Time: 130 ms (34.71%), Space: 44.4 MB (78.59%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.js
+++ 692-top-k-frequent-words/692-top-k-frequent-words.js
@@ -6,11 +6,10 @@
 
 var topKFrequent = function(words, k) {
     let hash = {}
-    let output = []
     for (let word of words) hash[word] = hash[word] ? hash[word] + 1 : 1
-    console.log(hash)
-    let sortedWords = Object.entries(hash).sort((a,b)=>b[1] - a[1] || (b[0] < a[0] ? 1 : -1))
-    return sortedWords.slice(0, k).map(([word]) => word)
+    let sorted = Object.entries(hash).sort((a,b)=> b[1] - a[1] || (b[0] < a[0] ? 1 : -1))
+    return sorted.splice(0, k).map(([word])=>word)
+
 };
 /*
 bbcddeeeaaa

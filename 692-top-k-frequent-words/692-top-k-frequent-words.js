20b1d83db92b47f91205288ad335b5c6a4bea7ab
SystemX <med.fellak@gmail.com>
2022-03-11 21:43:53 -0500

    Time: 124 ms (40.37%), Space: 44.8 MB (68.81%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.js
+++ 692-top-k-frequent-words/692-top-k-frequent-words.js
@@ -5,9 +5,13 @@
  */
 
 var topKFrequent = function(words, k) {
+    // a table to store frequency
     let hash = {}
+    // store words by frequency
     for (let word of words) hash[word] = hash[word] ? hash[word] + 1 : 1
+    // sort based on frequency first, then equal freq, get sorted alphabetic
     let sorted = Object.entries(hash).sort((a,b)=> b[1] - a[1] || (b[0] < a[0] ? 1 : -1))
+    // splice only up to k values, and map words out of the entries
     return sorted.splice(0, k).map(([word])=>word)
 
 };
